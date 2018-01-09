11e3222a27ac97c11fc183ae8f6634a376667c07
AlejandroRodriguezP <a.rodriguez.p.1995@gmail.com>
2018-01-09 16:22:43 -0600

    check if obj is arr

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,14 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var arr = [];
+  if(typeof obj === "number"||typeof obj === null || typeof obj=== "boolean"){
+    return obj + " " ;
+  }
+  else if(obj === "string"){
+    return obj;
+  }
+  else if(Array.isArray(obj)){
+    obj.forEach(stringify(element))
+  }
 };
